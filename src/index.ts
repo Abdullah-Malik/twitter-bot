99f18e81945d8e166e0f02dbc431882f1400d6ec
Abdullah Malik <abdullahmalik61@gmail.com>
2022-09-11 22:36:38 +0500

    exploring twitter-api-v2 package

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
-import getContentSourceFunction from './content_sources';
+import { TwitterApi } from 'twitter-api-v2';
+import * as dotenv from 'dotenv';
+// import getContentSourceFunction from './content_sources';
 
-const contentSource = getContentSourceFunction('Reddit');
-for (let i = 0; i < 500; i++) {
-  contentSource().then((post) => {
-    console.log(post);
-  });
-}
+dotenv.config();
+
+const client = new TwitterApi({
+  appKey: process.env.API_KEY || '',
+  appSecret: process.env.API_KEY_SECRET || '',
+  accessToken: process.env.ACCESS_TOKEN || '',
+  accessSecret: process.env.ACCESS_TOKEN_SECRET || '',
+});
+
+(async () => {
+  // const getContent = getContentSourceFunction('Reddit');
+  // const post = await getContent();
+  // console.log(post);
+  const res = await client.v1.destroyFriendship({ screen_name: 'elonmusk' });
+  console.log(res);
+})();
+
+export {};

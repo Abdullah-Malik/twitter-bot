2152dbf9239f147a3e7118ceb8b63216fd66a550
Abdullah Malik <abdullahmalik61@gmail.com>
2022-09-22 02:30:09 +0500

    Added UserV2 type and FetchedUser collection

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,22 +1,39 @@
 import mongoose, { Schema } from 'mongoose';
 
-export const userSchema = new Schema({
-  name: { type: String, required: true },
-  id_str: { type: String, required: true },
-  id: { type: Number, required: true },
-  screen_name: { type: String, required: true },
-  protected: { type: Boolean, required: true },
-  verified: { type: Boolean, required: true },
-  followers_count: { type: Number, required: true },
-  friends_count: { type: Number, required: true },
-  listed_count: { type: Number, required: true },
-  favourites_count: { type: Number, required: true },
-  statuses_count: { type: Number, required: true },
-  location: String,
-  url: String,
-  description: String,
-});
+export const userV1Schema = new Schema(
+  {
+    name: { type: String, required: true },
+    id_str: { type: String, required: true },
+    id: { type: Number, required: true },
+    screen_name: { type: String, required: true },
+    protected: { type: Boolean, required: true },
+    verified: { type: Boolean, required: true },
+    followers_count: { type: Number, required: true },
+    friends_count: { type: Number, required: true },
+    listed_count: { type: Number, required: true },
+    favourites_count: { type: Number, required: true },
+    statuses_count: { type: Number, required: true },
+    location: String,
+    url: String,
+    description: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export const User = mongoose.model('User', userSchema);
+export const userV2Schema = new Schema(
+  {
+    id: { type: String, required: true },
+    name: { type: String, required: true },
+    username: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export default User;
+export const SeedUser = mongoose.model('SeedUser', userV1Schema);
+export const FetchedUser = mongoose.model('FetchedUser', userV2Schema);
+
+export default SeedUser;
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,4 +1,5 @@
 import { InferSchemaType } from 'mongoose';
-import { userSchema } from '../models';
+import { userV1Schema, userV2Schema } from '../models';
 
-export type UserV1 = InferSchemaType<typeof userSchema>;
+export type UserV1 = InferSchemaType<typeof userV1Schema>;
+export type UserV2 = InferSchemaType<typeof userV2Schema>;

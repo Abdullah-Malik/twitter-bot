a24dc8b1cfb377a06e53aa7d3bee7affeb39476d
Abdullah Malik <abdullahmalik61@gmail.com>
2022-09-23 03:37:03 +0500

    changed cron expression

--- functions/send-tweet.ts
+++ functions/send-tweet.ts
@@ -5,7 +5,7 @@ import { client } from '../src/client';
 
 dotenv.config();
 
-export const handler = schedule('*/43 * * * *', async () => {
+export const handler = schedule('43 8-23 * * *', async () => {
   const getContent = getContentSourceFunction('Reddit');
   try {
     const post = await getContent();

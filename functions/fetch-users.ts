8d0099ca9063f3d01f6783c34602392df2473616
Abdullah Malik <abdullahmalik61@gmail.com>
2022-09-23 04:16:28 +0500

    updated fetch-users function

--- functions/fetch-users.ts
+++ functions/fetch-users.ts
@@ -10,17 +10,12 @@ export const handler = schedule('@hourly', async (event) => {
   connect();
 
   const users = await SeedUser.find();
-  const MAX_REQUESTS = 10;
-  let count = 0;
 
   if (users && users.length) {
     const firstUser = users[0];
     const followingPaginator = await client.v2.following(firstUser.id_str, { asPaginator: true });
 
-    while (!followingPaginator.done && count < MAX_REQUESTS) {
-      await followingPaginator.fetchNext();
-      count += 1;
-    }
+    await followingPaginator.fetchLast(1000);
 
     await FetchedUser.insertMany(followingPaginator.data.data, { ordered: false });
     await SeedUser.deleteOne({ screen_name: firstUser.screen_name });

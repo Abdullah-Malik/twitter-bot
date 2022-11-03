92fd775cda4be1c924c1b994aeaf13a987797fef
Abdullah Malik <50669245+Abdullah-Malik@users.noreply.github.com>
2022-11-03 05:52:08 +0500

    Update follow-user.ts

--- functions/follow-user.ts
+++ functions/follow-user.ts
@@ -6,7 +6,7 @@ import { User, Following } from '../src/models';
 
 dotenv.config();
 
-export const handler = schedule('*/12 * * * *', async (event) => {
+export const handler = schedule('*/8 * * * *', async (event) => {
   connect();
   let user;
 
